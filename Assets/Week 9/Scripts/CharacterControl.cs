9e41fd256d354451c36c3b8b185b301564f4a33d
hughjere <hughjere@OA-S143-09>
2024-03-13 18:39:20 -0400

    task 4b complete; ui is working :)

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -2,10 +2,19 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CharacterControl : MonoBehaviour
 {
     public static Villager SelectedVillager { get; private set; }
+    public GameObject findText;
+    public static TextMeshProUGUI selected;
+    void Start()
+    {
+        findText = GameObject.Find("findText");
+        selected = findText.GetComponent<TextMeshProUGUI>();
+    }
+
     public static void SetSelectedVillager(Villager villager)
     {
         if(SelectedVillager != null)
@@ -14,6 +23,7 @@ public class CharacterControl : MonoBehaviour
         }
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
+        selected.SetText(villager.CanOpen().ToString());
     }
-    
+
 }
